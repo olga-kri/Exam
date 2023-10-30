48ecd84aab31e6b142ede0c8695ce0ee965d5673
olga-kri <krivenkoolga1987@gmail.com>
2023-10-31 00:48:39 +0500

    Method square

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
 public class Main {
+    public static int square (int a) {
+        int result = a*a;
+        return result;
+    }
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int x = 178;
+        System.out.println("Квадрат числа " + x + " равен " + square(x));
     }
 }
